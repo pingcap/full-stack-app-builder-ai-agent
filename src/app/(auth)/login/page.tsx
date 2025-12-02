27d38839348a7249f53cc07ec8aa6dd31f339c72
Jagger <634750802@qq.com>
2025-12-02 15:43:28 +0800

    Revert "ui: refactor ui"
    This reverts commit 5e37f3a2ae53ebb9f0d4c6168292ebf92d64c2c3.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,22 +3,15 @@ import { LoginForm } from "@/components/login-form";
 export default async function LoginPage({
   searchParams,
 }: {
-  searchParams?: Promise<{
-    error?: string | string[];
-    callbackUrl?: string;
-  }>;
+  searchParams?: Promise<{ error?: string | string[]; callbackUrl?: string }>;
 }) {
   const { error, callbackUrl } = (await searchParams) ?? {};
-  const googleEnabled =
-    Boolean(process.env.GOOGLE_CLIENT_ID) &&
-    Boolean(process.env.GOOGLE_CLIENT_SECRET);
   return (
     <div className="w-screen min-h-screen bg-muted flex flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-sm">
         <LoginForm
           error={error ? String(error) : undefined}
           callbackUrl={callbackUrl}
-          googleEnabled={googleEnabled}
         />
       </div>
     </div>
