bc6255d27ff1303ca7e939a983922ce547e03686
Jagger <634750802@qq.com>
2025-12-02 19:21:39 +0800

    update system prompt

--- src/prompts/ui-session.ts
+++ src/prompts/ui-session.ts
@@ -24,7 +24,9 @@ The workflow is:
    - will commit to the working branch when finished
 
 Help user to generate the meta fields and first prompt:
-- This prompt will be sent to a coding agent like claude code or codex, don't plan too much details and create a huge prompt.
+- This prompt will be sent to a coding agent like claude code or codex.
+- Do not make a specific plan for generic job, let agent decide what to do.
+- Code agent can fetch URL content, let agent fetch URL if needed.
 - Make sure the coding agent will write and **execute the migration sql** if database features will be used
 - Do not use specific tech stack, agent needs to find it in the repository structure
 - \`npm run dev\` is running, the dev port is 3000.
