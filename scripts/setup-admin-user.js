c0c4414c8b7e5f922e051d6244d98a8f9eaf84d6
Jagger <634750802@qq.com>
2025-12-18 15:24:04 +0800

    fix

--- scripts/setup-admin-user.js
+++ scripts/setup-admin-user.js
@@ -3,11 +3,21 @@ import db from "../src/lib/db/db.ts";
 import { insert } from "../src/lib/kysely-utils.ts";
 
 await db.transaction().execute(async (trx) => {
-  await insert(trx, "user", {
+  const user = await insert(trx, "user", {
     name: "admin",
-    password: await hash("password", process.env.BCRYPT_SALT),
     avatar_url: "",
     email: "admin@example.com",
+    email_verified: 0,
+    role: "admin",
+  });
+
+  await insert(trx, "account", {
+    user_id: user.id,
+    account_id: "admin",
+    provider_id: "credential",
+    password: await hash("password", process.env.BCRYPT_SALT),
+    created_at: new Date(),
+    updated_at: new Date(),
   });
 });
 
